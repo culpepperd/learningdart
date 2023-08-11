27da4b0829e59f395a949a8c571324f42a9cf0bc
Daniel Culpepper <culpepperd2490@gmail.com>
2023-08-11 12:04:49 -0400

    finished Intro to Dart chapter at 2:01:00

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+String shortHandGetFullName(String firstName, String lastName) =>
+    '$firstName $lastName';
+
 String getFullName(String firstName, String lastName) {
-  return firstName + ' ' + lastName;
+  return '$firstName $lastName';
 }
 
 void main() {
@@ -15,6 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(shortHandGetFullName('Lindsey', 'Loo'));
     print(getFullName('Daniel', 'Culpepper'));
     return MaterialApp(
       title: 'Flutter Demo',
